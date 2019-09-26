d1c120107c31e6e057b0f1ea724c532cadd86b1c
Blythe Pollard <blytheepollard@gmail.com>
2019-09-26 17:42:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,9 +5,18 @@ class Dog
   def name
     @name
   end
-
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
 end  
 
 fido = Dog.new
 fido.name = "Fido"
  fido.name
+ 
+ snoopy = dog.new
+ snoopy.breed = "Beagle"
+ snoopy.breed
