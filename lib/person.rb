573df0a7d2a15b1d8f152c6b43c750e4d6084789
Blythe Pollard <blytheepollard@gmail.com>
2019-09-26 17:47:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,13 @@ class Person
     @name = person_name
   end
   def name
-    @this_persons_name
+    @name
   end
   def job=(person_job)
-    @this_persons_job = person_job
+    @job = person_job
   end
   def job
-    @this_persons_job
+    @job
   end
 end
 
